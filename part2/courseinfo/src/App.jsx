8b053bd3e31b6f3c8ba667d6549e99281e95c770
Mir Ilham <mhmilham3@gmail.com>
2025-09-30 23:30:14 +0600

    Add Part-2, Solution-5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map((part) => 
-      <Part key={part.id} part={part} />
-    )}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <h3>Number of exercises {props.total}</h3>
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={course.parts.reduce((s, part) => s + part.exercises, 0)}
-      />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
