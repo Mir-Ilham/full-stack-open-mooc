4c911629b94d293668d63e23641e793e12c975e5
Mir Ilham <mhmilham3@gmail.com>
2025-09-30 23:17:56 +0600

    Add Part-2, Solution-2,3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -21,13 +21,9 @@ const Course = ({course}) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      /> */}
+      <Total
+        total={course.parts.reduce((s, part) => s + part.exercises, 0)}
+      />
     </div>
   )
 }
