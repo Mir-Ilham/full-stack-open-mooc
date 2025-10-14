7e6d0bdb15bc9aa69092bdece99cd08f96029eae
Mir Ilham <mhmilham3@gmail.com>
2025-10-14 06:01:49 +0600

    Add Part-2, Solution-9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,28 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      phone: 420
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [query, setQuery] = useState('')
 
 
   const changeInputName = (event) => {
     setNewName(event.target.value)
   }
 
-  const changePhoneNumber = (event) => {
-    setNewPhoneNumber(event.target.value)
+  const changeNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const changeQuery = (event) => {
+    setQuery(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
 
     const personObject = {
+      id: persons.length + 1,
       name: newName,
-      phone: newPhoneNumber
+      number: newNumber
     }
 
     if (persons.find(person => person.name === newName)) {
@@ -33,22 +40,29 @@ const App = () => {
 
     setPersons(persons.concat(personObject))
     setNewName('')
-    setNewPhoneNumber('')
+    setNewNumber('')
   }
+
+  const personsToDisplay = persons.filter(person => {
+    const cleanQuery = query.trim().toLowerCase()
+    return person.name.toLowerCase().includes(cleanQuery)
+  })
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>newName : {newName}</div>
-      <div>newPhoneNumber : {newPhoneNumber}</div>
+      
+      <div>
+        Search: <input value={query} onChange={changeQuery}/>
+      </div>
 
-
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={changeInputName}/>
         </div>
         <div>
-          phone: <input value={newPhoneNumber} onChange={changePhoneNumber}/>
+          phone: <input value={newNumber} onChange={changeNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -56,8 +70,8 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) =>
-        <p key={person.name}>{person.name} {person.phone}</p>
+      {personsToDisplay.map((person) =>
+        <p key={person.id}>{person.name} {person.number}</p>
       )}
 
     </div>
