c1e33d81df2aac2831ab6c827d8b518694ffd693
Mir Ilham <mhmilham3@gmail.com>
2025-10-25 17:51:27 +0600

    Add Part-3, Solution-13

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -19,29 +21,6 @@ const format = ':method :url :status :res[content-length] - :response-time ms :p
 
 app.use(morgan(format));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
   const now = new Date()
   response.send(
@@ -52,7 +31,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -109,8 +90,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.json(match)
 })
 
-// const PORT = 3001
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
