c626c738a19d123fd45c5a20bb0a569acde2f1aa
Mir Ilham <mhmilham3@gmail.com>
2025-10-27 20:02:37 +0600

    Add Part-4, Solution-2. Structuring blog list app.

---
+++ part4/blog_list/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(mongoUrl)
+  .then(result => {
+    logger.info('Connected to MongoDB.')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('post-data', (request) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+  return ' '
+})
+
+const format = ':method :url :status :res[content-length] - :response-time ms :post-data'
+
+app.use(morgan(format))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
