f521945e8ef043b6b92531dd00a35512c73258ac
Mir Ilham <mhmilham3@gmail.com>
2025-10-31 20:54:21 +0600

    Organize tests into grouped sections based on CRUD operation type.

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -9,140 +9,150 @@ const blogsData = require('./blogs_data')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(blogsData)
-})
+describe('When there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(blogsData)
+  })
 
-test('All blogs returned', async () => {
-  const response = await api.get('/api/blogs')
+  describe('for read operations', () => {
+    test('all blogs returned', async () => {
+      const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, blogsData.length)
-})
+      assert.strictEqual(response.body.length, blogsData.length)
+    })
 
-test('Unique identifier property of blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
+    test('unique identifier property of blog posts is named id', async () => {
+      const response = await api.get('/api/blogs')
 
-  const blogs = response.body
-  const blog = blogs[0]
+      const blogs = response.body
+      const blog = blogs[0]
 
-  assert.ok(blog.id)
-  assert.strictEqual(blog._id, undefined)
-})
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
+  })
 
-test('A valid blog can be added ', async () => {
-  const newBlog =   {
-    title: 'React router',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 6,
-  }
+  describe('for create operations', () => {
+    test('a valid blog can be added ', async () => {
+      const newBlog =   {
+        title: 'React router',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 6,
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogs = await blogsInDb()
+      const blogs = await blogsInDb()
 
-  const titles = blogs.map(r => r.title)
+      const titles = blogs.map(r => r.title)
 
-  assert.strictEqual(blogs.length, blogsData.length + 1)
-  assert(titles.includes(newBlog.title))
-})
+      assert.strictEqual(blogs.length, blogsData.length + 1)
+      assert(titles.includes(newBlog.title))
+    })
 
-test('A blog added without likes count defaults to zero', async () => {
-  const newBlog =   {
-    title: 'Redux learning',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-  }
+    test('a blog added without likes count defaults to zero', async () => {
+      const newBlog =   {
+        title: 'Redux learning',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogs = await blogsInDb()
+      const blogs = await blogsInDb()
 
-  const savedBlog = blogs.find(blog => blog.title === newBlog.title)
+      const savedBlog = blogs.find(blog => blog.title === newBlog.title)
 
-  assert.strictEqual(blogs.length, blogsData.length + 1)
-  assert.strictEqual(savedBlog.likes, 0)
-})
+      assert.strictEqual(blogs.length, blogsData.length + 1)
+      assert.strictEqual(savedBlog.likes, 0)
+    })
 
-test('Attempting to add a blog without title or url is a bad request', async () => {
-  const newBlogNoTitle =   {
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/'
-  }
+    test('attempting to add a blog without title or url is a bad request', async () => {
+      const newBlogNoTitle =   {
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/'
+      }
 
-  const newBlogNoURL =   {
-    title: 'Redux learning',
-    author: 'Michael Chan'
-  }
+      const newBlogNoURL =   {
+        title: 'Redux learning',
+        author: 'Michael Chan'
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoTitle)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(newBlogNoTitle)
+        .expect(400)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoURL)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(newBlogNoURL)
+        .expect(400)
 
-  const blogs = await blogsInDb()
-  assert.strictEqual(blogs.length, blogsData.length)
-})
+      const blogs = await blogsInDb()
+      assert.strictEqual(blogs.length, blogsData.length)
+    })
+  })
 
-test('Deleting a specific blog works', async () => {
-  const blogToDelete = blogsData[0]
+  describe('for update operations', () => {
+    test('updating a specific blog works', async () => {
+      const blogToUpdate = blogsData[0]
+      blogToUpdate.likes = 101
 
-  await api
-    .delete(`/api/blogs/${blogToDelete._id}`)
-    .expect(200)
+      await api
+        .put(`/api/blogs/${blogToUpdate._id}`)
+        .send(blogToUpdate)
+        .expect(200)
 
-  const blogs = await blogsInDb()
-  const match = blogs.find(blog => blog.id === blogToDelete._id)
+      const blogs = await blogsInDb()
+      const match = blogs.find(blog => blog.id === blogToUpdate._id)
 
-  assert.strictEqual(match, undefined)
-  assert.strictEqual(blogs.length, blogsData.length - 1)
-})
+      assert.strictEqual(match.likes, blogToUpdate.likes)
+    })
 
-test('Attempting to delete a non-existent blog fails', async () => {
-  const blogToDelete = blogsData[0]
+    test('attempting to update a non-existent blog fails', async () => {
+      const blogToUpdate = blogsData[0]
 
-  await api
-    .delete(`/api/blogs/${blogToDelete._id + '1'}`)
-    .send(blogToDelete)
-    .expect(400)
-})
+      await api
+        .put(`/api/blogs/${blogToUpdate._id + '1'}`)
+        .send(blogToUpdate)
+        .expect(400)
+    })
+  })
 
-test('Updating a specific blog works', async () => {
-  const blogToUpdate = blogsData[0]
-  blogToUpdate.likes = 101
+  describe('for delete operations', () => {
+    test('deleting a specific blog works', async () => {
+      const blogToDelete = blogsData[0]
 
-  await api
-    .put(`/api/blogs/${blogToUpdate._id}`)
-    .send(blogToUpdate)
-    .expect(200)
+      await api
+        .delete(`/api/blogs/${blogToDelete._id}`)
+        .expect(200)
 
-  const blogs = await blogsInDb()
-  const match = blogs.find(blog => blog.id === blogToUpdate._id)
+      const blogs = await blogsInDb()
+      const match = blogs.find(blog => blog.id === blogToDelete._id)
 
-  assert.strictEqual(match.likes, blogToUpdate.likes)
-})
+      assert.strictEqual(match, undefined)
+      assert.strictEqual(blogs.length, blogsData.length - 1)
+    })
 
-test('Attempting to update a non-existent blog fails', async () => {
-  const blogToUpdate = blogsData[0]
+    test('attempting to delete a non-existent blog fails', async () => {
+      const blogToDelete = blogsData[0]
 
-  await api
-    .put(`/api/blogs/${blogToUpdate._id + '1'}`)
-    .send(blogToUpdate)
-    .expect(400)
+      await api
+        .delete(`/api/blogs/${blogToDelete._id + '1'}`)
+        .send(blogToDelete)
+        .expect(400)
+    })
+  })
 })
 
 after(async () => {
