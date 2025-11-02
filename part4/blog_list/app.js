9c77e4863a0c8ac1c367badb0d12fb616b6dd18a
Mir Ilham <mhmilham3@gmail.com>
2025-11-02 13:19:31 +0600

    Add Part-4, Solution-15. Implement User creation functionality and tests.

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -24,18 +25,21 @@ mongoose.connect(mongoUrl)
 app.use(express.json())
 app.use(cors())
 
-morgan.token('post-data', (request) => {
-  if (request.method === 'POST') {
-    return JSON.stringify(request.body)
-  }
-  return ' '
-})
+if (process.env.NODE_ENV !== 'test') {
+  morgan.token('post-data', (request) => {
+    if (request.method === 'POST') {
+      return JSON.stringify(request.body)
+    }
+    return ' '
+  })
 
-const format = ':method :url :status :res[content-length] - :response-time ms :post-data'
+  const format = ':method :url :status :res[content-length] - :response-time ms :post-data'
 
-app.use(morgan(format))
+  app.use(morgan(format))
+}
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
