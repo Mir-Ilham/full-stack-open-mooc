0f79fdba7867a70a3e8f24d7c35498afbcaed8aa
Mir Ilham <mhmilham3@gmail.com>
2025-11-02 14:32:34 +0600

    Add Part-4, Solution-17. Listing blogs alongs with user info and vice versa.

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,21 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
+  const user = await User.findOne()
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   if (!blog.likes)
     blog.likes = 0
+
+  blog.user = user._id
 
   if (!blog.title || !blog.url)
     return response.status(400).end()
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
