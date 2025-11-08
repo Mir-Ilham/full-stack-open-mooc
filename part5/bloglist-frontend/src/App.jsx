37c203ca77462d153be123df0e4578b9839b0237
Mir Ilham <mhmilham3@gmail.com>
2025-11-08 17:07:49 +0600

    Add Part-5, Solution-3. Implement create blog frontend UI for logged in users.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
+import CreateBlogForm from './components/CreateBlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  // States relating to login
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
+
+  // States relating to blog creation
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('') 
+  const [url, setUrl] = useState('') 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -22,6 +30,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -34,7 +43,7 @@ const App = () => {
       window.localStorage.setItem(
         'loggedUser', JSON.stringify(user)
       )
-
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -51,13 +60,48 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateBlog = async event => {
+    event.preventDefault()
+
+    if (!author || !title || !url) {
+      setErrorMessage('Complete all blog entries')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      return
+    }
+
+    try {
+      const blog = await blogService.create({ title, author, url })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs(blogs.concat(blog))
+    } catch (exception) {
+      setErrorMessage(`Error: ${exception.message}`)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       {errorMessage && <p>{errorMessage}</p>}
       {user ?
         <>
           <h2>Blogs</h2>
+
           <div>{user.username} is logged in <button onClick={() => handleLogout()}>Logout</button></div>
+
+          <CreateBlogForm handleCreateBlog={handleCreateBlog} 
+                    title={title} 
+                    setTitle={setTitle} 
+                    author={author} 
+                    setAuthor={setAuthor}
+                    url={url}
+                    setUrl={setUrl}/>
+
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
---
+++ part5/bloglist-frontend/src/components/CreateBlogForm.jsx
@@ -0,0 +1,42 @@
+const CreateBlogForm = ({handleCreateBlog, title, setTitle, author, setAuthor, url, setUrl}) => {
+  return (
+    <>
+      <h2>Create New</h2>
+      <form onSubmit={handleCreateBlog}>
+        <div>
+          <label>
+            title:
+            <input
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:
+            <input
+              type="text"
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:
+            <input
+              type="text"
+              value={url}
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
+
+export default CreateBlogForm
