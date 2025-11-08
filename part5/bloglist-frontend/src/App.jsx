69cc400191af0e25475b56278c1aa8f6579476e1
Mir Ilham <mhmilham3@gmail.com>
2025-11-08 15:39:22 +0600

    Add Part-5, Solution-1. Implement bloglist frontend login functionality.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +17,41 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {errorMessage && <p>{errorMessage}</p>}
+      {user ?
+        <>
+          <h2>Blogs</h2>
+          <p>{user.username} is logged ingit</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </>
+        :
+        <LoginForm handleLogin={handleLogin} 
+                  username={username} 
+                  setUsername={setUsername} 
+                  password={password} 
+                  setPassword={setPassword}/>
+      }
+
     </div>
   )
 }
