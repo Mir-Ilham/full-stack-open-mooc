0d6f507cd071cb1b283736098fc004ef7d0db93f
Mir Ilham <mhmilham3@gmail.com>
2025-11-12 10:57:04 +0600

    Add Part-5, Solution-18. Implement tests for successful and failed login.

--- part5/bloglist-e2e/tests/bloglist_app.spec.js
+++ part5/bloglist-e2e/tests/bloglist_app.spec.js
@@ -1,12 +1,44 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        username: 'Ilham',
+        name: 'MHM Ilham',
+        password: 'pforpassword'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
     const locator = page.getByText('Login')
     await expect(locator).toHaveCount(2)
+  })
+
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('Ilham')
+      await page.getByLabel('password').fill('pforpassword')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Ilham is logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('Ilham')
+      await page.getByLabel('password').fill('dforpassword')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+      await expect(page.getByText('Ilham is logged in')).not.toBeVisible()
+    })
   })
 })
